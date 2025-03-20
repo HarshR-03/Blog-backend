fc054fd2fafcab1ec51f93c987a7026bb2e18285
HarshR-03 <152909344+HarshR-03@users.noreply.github.com>
2025-03-20 21:10:33 +0530

    added 'zekeyeager18.pythonanywhere.com' to ALLOWED_HOSTS

--- simpleblog/settings.py
+++ simpleblog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0$^01e!_q!fyd)it02-(&kbk*y^y3f0ztq-)jllqv*wvb8xbl5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['zekeyeager18.pythonanywhere.com']
 
 
 # Application definition
